93dcee37bc1eff2ff031815ad081f2a228e23a6d
fbclh <fbclh@protonmail.com>
2022-02-20 08:42:49 -0300

    Finish 1 and 2 chapters

---
+++ 1-values-types-operators/summary.js
@@ -0,0 +1,81 @@
+// Arithmetic
+100 + 4 * 11;
+(100 + 4) * 11;
+
+// Special numbers
+Infinity;
+-Infinity;
+NaN;
+
+// Strings
+`Down on the sea`
+"Lie on the ocean"
+'Float on the ocean'
+`half of 100 is ${100 / 2}`
+
+// Unary operators
+console.log(typeof 4.5) // → number
+console.log(typeof "x") // → string
+console.log(- (10 - 2)) // → -8
+
+// Boolean values
+console.log(3 > 2) // → true
+console.log(3 < 2) // → false
+
+console.log("Aardvark" < "Zoroaster") // → true
+console.log("Itchy" != "Scratchy") // → true
+console.log("Apple" == "Orange") // → false
+
+// Logical operators
+console.log(true && false) // → false
+console.log(true && true) // → true
+
+console.log(false || true) // → true
+console.log(false || false) // → false
+
+console.log(true ? 1 : 2); // → 1
+console.log(false ? 1 : 2); // → 2
+
+// Empty values
+null;
+undefined;
+
+
+// Automatic type conversion
+console.log(8 * null) // → 0
+console.log("5" - 1) // → 4
+console.log("5" + 1) // → 51
+console.log("five" * 2) // → NaN
+console.log(false == 0) // → true
+
+console.log(null == undefined); // → true
+console.log(null == 0); // → false
+
+// “Short-circuiting of logical operators
+console.log(null || "user") // → user
+console.log("Agnes" || "user") // → Agnes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2-program-structure/exercises.js
@@ -0,0 +1,96 @@
+// Looping a triangle
+
+/*
+  Write a loop that makes seven calls to console.log to output the following triangle:
+
+  #
+  ##
+  ###
+  ####
+  #####
+  ######
+  #######
+
+  It may be useful to know that you can find the length of a string by writing .length after it.
+
+  let abc = "abc";
+  console.log(abc.length); // → 3
+*/
+
+for (let hash = '#'; hash.length <= 7; hash += '#') {
+  console.log(hash);
+}
+
+//
+
+// FizzBuzz
+
+/* 
+  Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+
+  When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+
+  (This is actually an interview question that has been claimed to weed out a significant percentage of programmer candidates. So if you solved it, your labor market value just went up.)
+*/
+
+function FizzBuzz() {
+  let arr = [];
+
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) arr.push('FizzBuzz');
+    else if (i % 3 === 0) arr.push('Fizz');
+    else if (i % 5 === 0) arr.push('Buzz');
+    else arr.push(i);
+  }
+
+  return arr;
+}
+
+console.log(FizzBuzz(100));
+
+//
+
+// Chessboard
+
+/* 
+  Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+
+  Passing this string to console.log should show something like this:
+
+   # # # #
+  # # # # 
+   # # # #
+  # # # # 
+   # # # #
+  # # # # 
+   # # # #
+  # # # #
+*/
+
+let size = 8;
+for (let i = 0; i < size; i++) {
+  let line = ' ';
+
+  for (let j = 0; j < size; j++) {
+    line += (i + j + 1) % 2 ? ' ' : '#';
+  }
+
+  console.log(line);
+}
+
+// let size = 8;
+
+// let board = '';
+
+// for (let y = 0; y < size; y++) {
+//   for (let x = 0; x < size; x++) {
+//     if ((x + y) % 2 == 0) {
+//       board += ' ';
+//     } else {
+//       board += '#';
+//     }
+//   }
+//   board += '\n';
+// }
+
+// console.log(board);
